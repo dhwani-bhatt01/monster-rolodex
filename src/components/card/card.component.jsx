d07740f4ee9a42ffbcd67f421471d52716f5ab1c
Dhwani Bhatt <bhattdhwani01@gmail.com>
2021-03-19 11:57:47 +0530

    monsters-rolodex completed

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,7 +6,8 @@ export const Card = (props) => {
     return (
         <div className = 'card-container'>
             <img className = 'img-resize' alt = 'monster' src = {`https://robohash.org/${props.monster.id}?set=set2`} />
-            <h1> {props.monster.name} </h1>
+            <h2> {props.monster.name} </h2>
+            <p> {props.monster.name}</p>
         </div>
     )
 }
